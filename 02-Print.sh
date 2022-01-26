ed5a61d14874ccd32f74a84216a3865bf788d893
Hareesh <bandihareesh7@gmail.com>
2022-01-26 14:27:05 +0530

    adding color code examples

--- 02-Print.sh
+++ 02-Print.sh
@@ -10,3 +10,24 @@
 ## syntaxes: echo <MESSAGE TO PRINT>
 
 echo welcome hello world
+
+## syntaxes for color or new lines : echo  -e "MESSAGE/n/t/e"
+echo -e option do need message to be quoted (enter single or double quotes) | (without quotes also work but more syntaxing)
+
+# Print Multiple Lines (/n), Some tab space (/t)
+
+echo Hello World,/n/tWelcome
+
+# Enable Colors (/e)
+
+# syntax: echo -e "COLOR-CODEmMESSAGE"
+# COLORS AND THEIR CODES ARE
+# RED       31
+# GREEN     32
+# YELLOW    33
+# BLUE      34
+# MAGENTA   35
+# CYAN      36
+
+echo -e "\e[31mMESSAGE IN RED"
+echo -e "\e[33mMESSAGE IN YELLOW"
