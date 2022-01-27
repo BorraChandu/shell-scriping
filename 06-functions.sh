b9a114a8d60dd3b23855112604c56758f865dae5
Hareesh <bandihareesh7@gmail.com>
2022-01-27 18:27:12 +0530

    functions

---
+++ 06-functions.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+
+## decleare a function
+sample() {
+  echo hello i am a sample function
+  echo value of a = ${a}
+  b=200
+}
+
+## main program
+
+#call the function
+a=100
+sample
+echo value of b=${b}
+echo first argument in main program = $1
