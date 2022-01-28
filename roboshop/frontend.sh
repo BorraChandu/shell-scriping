a1b1e023031b67100935a645397c12d715437513
Hareesh <bandihareesh7@gmail.com>
2022-01-28 16:34:27 +0530

    functions

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -2,27 +2,35 @@
 
 source common.sh
 
-PRINT "Installing Nginx"
+PRINT "Installing Nginx\t"
 yum install nginx -y &>>$LOG
 STAT_CHECK $?
 
-PRINT "Download Frontend"
+PRINT "Download Frontend\t"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG
 STAT_CHECK $?
 
-PRINT "Remove Old History Docs"
-cd /usr/share/nginx/html  && rm -rf *   &>>$LOG
+
+PRINT "Remove Old HtDocs\t"
+cd /usr/share/nginx/html &>>$LOG && rm -rf * &>>$LOG
 STAT_CHECK $?
 
 PRINT "Extract Frontend Archive"
-unzip /tmp/frontend.zip  &>>$LOG && mv frontend-main/* .&>>$LOG && mv static/* . &>>$LOG && rm -rf frontend-master static &>>$LOG
+unzip /tmp/frontend.zip &>>$LOG && mv frontend-main/* . &>>$LOG && mv static/* . &>>$LOG  && rm -rf frontend-master static &>>$LOG
 STAT_CHECK $?
 
-PRINT "Update Roboshop Config/t"
+PRINT "Copy RoboShop Config\t"
 mv localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG
 STAT_CHECK $?
 
-PRINT "Starting Nginx/t/t"
+PRINT "Update RoboShop Config\t"
+sed -i -e '/catalogue/ s/localhost/catalogue.roboshop.internal/' -e '/user/ s/localhost/user.roboshop.internal/'  -e '/cart/ s/localhost/cart.roboshop.internal/' -e '/shipping/ s/localhost/shipping.roboshop.internal/' -e '/payment/ s/localhost/payment.roboshop.internal/' /etc/nginx/default.d/roboshop.conf
+STAT_CHECK $?
 
-systemctl restart nginx &>>$LOG
+PRINT "Enabling Nginx\t\t"
+systemctl enable nginx  &>>$LOG
+STAT_CHECK $?
+
+PRINT "Starting Nginx\t\t"
+systemctl restart nginx  &>>$LOG
 STAT_CHECK $?
