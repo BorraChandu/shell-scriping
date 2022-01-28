a76d1d379c2105919d44ed9fa7d6c4eeacafaa1d
Hareesh <bandihareesh7@gmail.com>
2022-01-28 16:22:05 +0530

    functions

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -1,3 +1,28 @@
 #!/bin/bash
 
-yum install nginx -y
+source common.sh
+PRINT "Installing Nginx"
+
+yum install nginx -y &>>$LOG
+STAT_CHECK $?
+
+PRINT "Download Frontend"
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG
+STAT_CHECK $?
+
+PRINT "Remove Old History Docs"
+cd /usr/share/nginx/html  && rm -rf *   &>>$LOG
+STAT_CHECK $?
+
+PRINT "Extract Frontend Archive"
+unzip /tmp/frontend.zip  &>>$LOG && mv frontend-main/* .&>>$LOG && mv static/* . &>>$LOG && rm -rf frontend-master static &>>$LOG
+STAT_CHECK $?
+
+PRINT "Update Roboshop Config/t"
+mv localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG
+STAT_CHECK $?
+
+PRINT "Starting Nginx/t/t"
+
+systemctl restart nginx &>>$LOG
+STAT_CHECK $?
